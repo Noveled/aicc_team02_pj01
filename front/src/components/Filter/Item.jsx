33ef9afd9ed0900b1dbbe86715473822dbb8e4fc
sonjuhyeon <ths120521113105@daum.net>
2024-08-29 19:25:02 +0900

    이미지 수정

--- front/src/components/Filter/Item.jsx
+++ front/src/components/Filter/Item.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
-import { FaThumbsUp } from "react-icons/fa";
 
 const Item = ({ item }) => {
-  // console.log(item);
-  const deleteItem = () => {};
+  console.log(item);
 
   return (
     <div className="item w-full p-3 overflow-x-auto">
       <div className="rounded-lg p-2 bg-sky-200 w-full">
         <Link to={"/detail"} state={{ item: item }}>
           <div className="flex flex-col gap-y-1 min-w-[155px]">
-            <img src={item.img_url} alt="course_img" className="" />
+            <div className="h-[155px]">
+              <img
+                src={item.img_url}
+                alt="course_img"
+                className="object-cover w-full h-full"
+              />
+            </div>
             <h5 className="truncate">제목: {item.course_name}</h5>
             <p className="truncate">내용: {item.content}</p>
             <div className="flex justify-between gap-x-[2px]">
@@ -21,8 +25,8 @@ const Item = ({ item }) => {
             </div>
             <div className="flex justify-between">
               <div className="text-xs truncate">거리: {item.distance}km</div>
-              <div className="text-sm flex items-center gap-x-1">
-                <FaThumbsUp /> <div>{item.liked}</div>
+              <div className="text-xs flex items-center gap-x-1 truncate">
+                조회수: {item.viewcount}
               </div>
             </div>
           </div>
