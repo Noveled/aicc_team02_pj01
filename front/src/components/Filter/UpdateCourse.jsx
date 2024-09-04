a952656532855176011fd134e9f1f70a2d6e51b5
sonjuhyeon <ths120521113105@daum.net>
2024-09-04 17:33:58 +0900

    refactoring

--- front/src/components/Filter/UpdateCourse.jsx
+++ front/src/components/Filter/UpdateCourse.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Select from "react-select";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import axios from "axios";
 
 import {
   ChevronLeft,
@@ -15,6 +16,7 @@ import {
 } from "lucide-react";
 
 import "../../actions.css";
+import { city_options } from "../../utils/data";
 import { fetchUpdateCourse } from "../../redux/slices/apiSlice";
 
 //
@@ -31,6 +33,7 @@ const UpdateCourse = () => {
   const dispatch = useDispatch();
   // const [userData, setUserData] = useState();
   const userData = useSelector((state) => state.userInfoState);
+  const ct_options = city_options;
 
   // 기본 업로드 이미지
   const defaultImgUrl =
@@ -97,9 +100,7 @@ const UpdateCourse = () => {
       clearAllMarkers();
       course_info.waypoint.forEach(({ Ma, La }, index) => {
         const position = new kakao.maps.LatLng(Ma, La);
-        addMarker(
-          position
-        );
+        addMarker(position);
         addLinePath(position);
       });
     };
@@ -559,46 +560,17 @@ const UpdateCourse = () => {
               <span className="text-base text-[#111111] font-semibold py-1">
                 지역
               </span>
-              <div>
-                <select
-                  name="city"
-                  id="city"
-                  className="form-control"
-                  value={values.city}
-                  onChange={(e) =>
-                    setValues({ ...values, city: e.target.value })
-                  }
-                >
-                  <option value="" disabled>
-                    선택
-                  </option>
-                  <option value="강남구">강남구</option>
-                  <option value="강동구">강동구</option>
-                  <option value="강북구">강북구</option>
-                  <option value="강서구">강서구</option>
-                  <option value="관악구">관악구</option>
-                  <option value="광진구">광진구</option>
-                  <option value="구로구">구로구</option>
-                  <option value="금천구">금천구</option>
-                  <option value="노원구">노원구</option>
-                  <option value="도봉구">도봉구</option>
-                  <option value="동대문구">동대문구</option>
-                  <option value="동작구">동작구</option>
-                  <option value="마포구">마포구</option>
-                  <option value="서대문구">서대문구</option>
-                  <option value="서초구">서초구</option>
-                  <option value="성동구">성동구</option>
-                  <option value="성북구">성북구</option>
-                  <option value="송파구">송파구</option>
-                  <option value="양천구">양천구</option>
-                  <option value="영등포구">영등포구</option>
-                  <option value="용산구">용산구</option>
-                  <option value="은평구">은평구</option>
-                  <option value="종로구">종로구</option>
-                  <option value="중구">중구</option>
-                  <option value="중랑구">중랑구</option>
-                </select>
-              </div>
+              <Select
+                options={ct_options}
+                styles={{
+                  control: (baseStyles, state) => ({
+                    ...baseStyles,
+                    borderColor: state.isFocused ? "purple" : "#7c5ecf",
+                  }),
+                }}
+                defaultInputValue={values.city}
+                onChange={(e) => setValues({ ...values, city: e.value })}
+              ></Select>
             </div>
 
             <div>
--- front/src/components/NaviBar/MakeCourse.jsx
+++ front/src/components/NaviBar/MakeCourse.jsx
@@ -584,46 +584,6 @@ const MakeCourse = () => {
                 }}
                 onChange={(e) => setValues({ ...values, city: e.value })}
               ></Select>
-              {/* <div>
-                <select
-                  name="city"
-                  id="city"
-                  className="form-control"
-                  value={values.city}
-                  onChange={(e) =>
-                    setValues({ ...values, city: e.target.value })
-                  }
-                >
-                  <option value="" disabled>
-                    선택
-                  </option>
-                  <option value="강남구">강남구</option>
-                  <option value="강동구">강동구</option>
-                  <option value="강북구">강북구</option>
-                  <option value="강서구">강서구</option>
-                  <option value="관악구">관악구</option>
-                  <option value="광진구">광진구</option>
-                  <option value="구로구">구로구</option>
-                  <option value="금천구">금천구</option>
-                  <option value="노원구">노원구</option>
-                  <option value="도봉구">도봉구</option>
-                  <option value="동대문구">동대문구</option>
-                  <option value="동작구">동작구</option>
-                  <option value="마포구">마포구</option>
-                  <option value="서대문구">서대문구</option>
-                  <option value="서초구">서초구</option>
-                  <option value="성동구">성동구</option>
-                  <option value="성북구">성북구</option>
-                  <option value="송파구">송파구</option>
-                  <option value="양천구">양천구</option>
-                  <option value="영등포구">영등포구</option>
-                  <option value="용산구">용산구</option>
-                  <option value="은평구">은평구</option>
-                  <option value="종로구">종로구</option>
-                  <option value="중구">중구</option>
-                  <option value="중랑구">중랑구</option>
-                </select>
-              </div> */}
             </div>
 
             <div>
@@ -638,15 +598,6 @@ const MakeCourse = () => {
                 }
               />
             </div>
-            {/* <input
-            className="border border-gray-400 rounded-md"
-            type="text"
-            name="url"
-            placeholder="이미지 URL"
-            value={values.url}
-            onChange={(e) => setValues({ ...values, url: e.target.value })}
-          /> */}
-
             <input
               className="border border-none text-white py-1 bg-[#7c5ecf] rounded-md px-2 cursor-pointer"
               type="submit"
