4b7434a59a366ea9a3363db059ed8b3e5d55d1b4
sonjuhyeon <ths120521113105@daum.net>
2024-09-04 14:51:51 +0900

    add skeleton

--- front/src/components/Filter/Like.jsx
+++ front/src/components/Filter/Like.jsx
@@ -1,15 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Item from "./Item";
+import ItemLoadingSkeleton from "./ItemLoadingSkeleton";
 
 import { useDispatch, useSelector } from "react-redux";
 
 import { fetchGetUserJoinCourseData } from "../../redux/slices/apiSlice";
 import { fetchGetUsersData } from "../../redux/slices/usersSlice";
 
+import { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const Like = () => {
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
-  const userId = useSelector((state) => state.auth.authData.name);
+  const userId = useSelector((state) => state.userInfoState.userInfo.user_id);
   const myCourse = useSelector((state) => state.api.myCourse);
 
   useEffect(() => {
@@ -21,21 +26,32 @@ const Like = () => {
     const fetchGetCourse = async () => {
       try {
         await dispatch(fetchGetUserJoinCourseData(userId)).unwrap();
+        setLoading(true);
       } catch (error) {
         console.log("Failed to fetch items:", error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchGetCourse();
   }, [dispatch, userId]);
 
   return (
-    <div className="my-course bg-purple-50">
+    <div className="my-course bg-violet-50">
       <div className="grid grid-cols-2 w-full">
-        {myCourse
-          ?.filter((item) => item.is_visible === true)
-          ?.map((item, idx) => (
-            <Item key={idx} item={item}></Item>
-          ))}
+        {loading ? (
+          <SkeletonTheme baseColor="#ddd6fe" highlightColor="#c4b5fd">
+            <ItemLoadingSkeleton></ItemLoadingSkeleton>
+            <ItemLoadingSkeleton></ItemLoadingSkeleton>
+            <ItemLoadingSkeleton></ItemLoadingSkeleton>
+            <ItemLoadingSkeleton></ItemLoadingSkeleton>
+          </SkeletonTheme>
+        ) : (
+          myCourse
+            ?.filter((item) => item.is_visible === true)
+            ?.map((item, idx) => <Item key={idx} item={item}></Item>)
+        )}
+        {}
       </div>
     </div>
   );
--- front/src/components/Filter/MyCourse.jsx
+++ front/src/components/Filter/MyCourse.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Item from "./Item";
+import ItemLoadingSkeleton from "./ItemLoadingSkeleton";
 
 import { useDispatch, useSelector } from "react-redux";
 
 import { fetchGetUserJoinCourseData } from "../../redux/slices/apiSlice";
 import { fetchGetUsersData } from "../../redux/slices/usersSlice";
 
+import { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const MyCourse = () => {
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
   const userId = useSelector((state) => state.userInfoState.userInfo.user_id);
@@ -21,8 +26,11 @@ const MyCourse = () => {
     const fetchGetCourse = async () => {
       try {
         await dispatch(fetchGetUserJoinCourseData(userId)).unwrap();
+        setLoading(true);
       } catch (error) {
         console.log("Failed to fetch items:", error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchGetCourse();
@@ -31,11 +39,19 @@ const MyCourse = () => {
   return (
     <div className="my-course bg-violet-50">
       <div className="grid grid-cols-2 w-full">
-        {myCourse
-          ?.filter((item) => item.is_visible === true)
-          ?.map((item, idx) => (
-            <Item key={idx} item={item}></Item>
-          ))}
+        {loading ? (
+          <SkeletonTheme baseColor="#ddd6fe" highlightColor="#c4b5fd">
+            <ItemLoadingSkeleton></ItemLoadingSkeleton>
+            <ItemLoadingSkeleton></ItemLoadingSkeleton>
+            <ItemLoadingSkeleton></ItemLoadingSkeleton>
+            <ItemLoadingSkeleton></ItemLoadingSkeleton>
+          </SkeletonTheme>
+        ) : (
+          myCourse
+            ?.filter((item) => item.is_visible === true)
+            ?.map((item, idx) => <Item key={idx} item={item}></Item>)
+        )}
+        {}
       </div>
     </div>
   );
