78135b862b4a1f124f607915848471108187e786
sonjuhyeon <ths120521113105@daum.net>
2024-09-05 12:19:20 +0900

    deleted useless file in back

--- back/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { postUser, loginUser } = require("../controllers/postUser");
-
-router.post("/register", postUser);
-router.post("/login", loginUser);
-
-module.exports = router;
